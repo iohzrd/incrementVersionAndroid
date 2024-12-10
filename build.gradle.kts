c316892fdcdbf95539bd89060ab173a52189f581
iohzrd <synergysymphony@gmail.com>
2024-12-10 08:54:29 -0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,5 +4,6 @@ task("copyPreCommitGitHookOnBuild") {
     println("Copying pre-commit Git Hook Script")
     exec {
         commandLine("cp", "./scripts/pre-commit", "./.git/hooks")
+        commandLine("chmod", "+x", "./.git/hooks/pre-commit")
     }
 }
